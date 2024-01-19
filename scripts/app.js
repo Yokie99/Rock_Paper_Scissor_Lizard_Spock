6fc6ed26471cc664198b72fa1665481c3458eba5
Yokie99 <kyleym@uci.edu>
2024-01-18 20:46:35 -0800

    wins lose logic

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,20 @@
 //Logic Variables 
+let gameEnd = false;
 let players = 0;
 let rounds = 0;
 
 let p1Choice = "rock";
 let p2Choice = "rock";
+let p1Points = 0;
+let p2Points = 0;
 
 //player html tags
 let player1 = document.getElementById("player1");
 let player2 = document.getElementById("player2");
+
+let p1Status = document.getElementById("p1Status");
+let p2Status = document.getElementById("p2Status");
+let result = document.getElementById("result");
 
 
 //display divs
@@ -35,7 +42,7 @@ let lizardP1 = document.getElementById("lizardP1");
 let spockP1 = document.getElementById("spockP1");
 
 let goBtn = document.getElementById("goBtn");
-let homeBtn= document.getElementById("homeBtn");
+let homeBtn = document.getElementById("homeBtn");
 let gameplayRulesBtn = document.getElementById("gameplayRulesBtn");
 
 
@@ -53,14 +60,14 @@ function showHome() {
     gameplayDiv.classList.add("off");
     body.classList.remove("gameBG");
     body.classList.add("homeBG");
-  }
+}
 function showRounds() {
     homeDiv.classList.add("off");
     roundsDiv.classList.remove("off");
     gameplayDiv.classList.add("off");
     body.classList.remove("gameBG");
     body.classList.add("homeBG");
-  }
+}
 function showGameplay() {
     homeDiv.classList.add("off");
     roundsDiv.classList.add("off");
@@ -69,87 +76,254 @@ function showGameplay() {
     body.classList.add("gameBG");
 }
 
-function resetGame(){
+function resetGame() {
     players = 0;
     rounds = 0;
+    p1Status.innerText = '';
+    p2Status.innerText = '';
+
 }
 
 
 //Eventlisteners
 //Home screen
-OneVOneBtn.addEventListener("click", () =>{
+OneVOneBtn.addEventListener("click", () => {
     players = 2;
     showRounds();
 });
-cpuBtn.addEventListener("click", () =>{
+cpuBtn.addEventListener("click", () => {
     players = 1;
     showRounds();
 });
 //Rounds Screen
-BoOne.addEventListener("click", () =>{
+BoOne.addEventListener("click", () => {
     rounds = 1;
     showGameplay();
 });
-BoFive.addEventListener("click", () =>{
+BoFive.addEventListener("click", () => {
     rounds = 5;
     showGameplay();
 });
-BoSeven.addEventListener("click", () =>{
+BoSeven.addEventListener("click", () => {
     rounds = 7;
     showGameplay();
 });
 
 //Game Screen
-homeBtn.addEventListener("click", () =>{
+homeBtn.addEventListener("click", () => {
     resetGame();
     showHome();
 });
-goBtn.addEventListener("click", () =>{
-    player1.classList.remove("glow");
-    player2.classList.remove("glow");
+goBtn.addEventListener("click", () => {
+    checkWin();
 
 });
 
 //player1 btns
-rockP1.addEventListener("click", () =>{
+rockP1.addEventListener("click", () => {
     p1Choice = "rock";
-    player1.classList.add("glow")
+    p1Status.innerText = "Choice has been picked! rock";
+    p1Status.classList.add("glow");
 });
-paperP1.addEventListener("click", () =>{
+paperP1.addEventListener("click", () => {
     p1Choice = "paper";
-    player1.classList.add("glow")
+    p1Status.innerText = "Choice has been picked! paper";
+    p1Status.classList.add("glow");
 });
-scissorsP1.addEventListener("click", () =>{
+scissorsP1.addEventListener("click", () => {
     p1Choice = "scissors";
-    player1.classList.add("glow")
+    p1Status.innerText = "Choice has been picked! sciu";
+    p1Status.classList.add("glow");
 });
-lizardP1.addEventListener("click", () =>{
+lizardP1.addEventListener("click", () => {
     p1Choice = "lizard";
-    player1.classList.add("glow")
+    p1Status.innerText = "Choice has been picked! liz";
+    p1Status.classList.add("glow");
 });
-spockP1.addEventListener("click", () =>{
+spockP1.addEventListener("click", () => {
     p1Choice = "spock";
-    player1.classList.add("glow")
+    p1Status.innerText = "Choice has been picked! spock";
+    p1Status.classList.add("glow");
 });
 
 //player 2 btns
-rockP2.addEventListener("click", () =>{
+rockP2.addEventListener("click", () => {
     p2Choice = "rock";
-    player2.classList.add("glow")
+    p2Status.innerText = "Choice has been picked! Rock";
+    p2Status.classList.add("glow");
 });
-paperP2.addEventListener("click", () =>{
+paperP2.addEventListener("click", () => {
     p2Choice = "paper";
-    player2.classList.add("glow")
+    p2Status.innerText = "Choice has been picked! Paper";
+    p2Status.classList.add("glow");
 });
-scissorsP2.addEventListener("click", () =>{
+scissorsP2.addEventListener("click", () => {
     p2Choice = "scissors";
-    player2.classList.add("glow")
+    p2Status.innerText = "Choice has been picked! sci";
+    p2Status.classList.add("glow");
 });
-lizardP2.addEventListener("click", () =>{
+lizardP2.addEventListener("click", () => {
     p2Choice = "lizard";
-    player2.classList.add("glow")
+    p2Status.innerText = "Choice has been picked! liz";
+    p2Status.classList.add("glow");
 });
-spockP2.addEventListener("click", () =>{
+spockP2.addEventListener("click", () => {
     p2Choice = "spock";
-    player2.classList.add("glow")
+    p2Status.innerText = "Choice has been picked! spock";
+    p2Status.classList.add("glow");
 });
+
+
+//Game Logic for 1v1
+function checkWin() {
+    //p1 picks rock
+    switch (p1Choice == "rock") {
+        case (p2Choice == "rock"):
+            result.innerText = "Its a Tie!";
+
+            break;
+        case (p2Choice == "paper"):
+            p1Status.innerText = "You lost a point!";
+            p2Status.innerText = "You won a point!";
+            result.innerText = "Paper beats Rock!";
+            break;
+        case (p2Choice == "scissors"):
+            p1Status.innerText = "You won a point!";
+            p2Status.innerText = "You lost a point!";
+            result.innerText = "Rock beats Scissors!";
+            break;
+        case (p2Choice == "lizard"):
+            p1Status.innerText = "You won a point!";
+            p2Status.innerText = "You lost a point!";
+            result.innerText = "Rock beats Lizard!";
+            break;
+        case (p2Choice == "spock"):
+            p1Status.innerText = "You lost a point!";
+            p2Status.innerText = "You won a point!";
+            result.innerText = "Spock beats Rock!";
+            break;
+        default:
+            console.log("Rock was not picked by p1")
+    }
+
+    //p1 picks paper
+    switch (p1Choice == "paper") {
+        case (p2Choice == "rock"):
+            p1Status.innerText = "You won a point!";
+            p2Status.innerText = "You lost a point!";
+            result.innerText = "Paper beats Rock!";
+
+            break;
+        case (p2Choice == "paper"):
+            result.innerText = "Its a Tie!";
+
+            break;
+        case (p2Choice == "scissors"):
+            p1Status.innerText = "You lost a point!";
+            p2Status.innerText = "You won a point!";
+            result.innerText = "Scissors beats paper!";
+            break;
+        case (p2Choice == "lizard"):
+            p1Status.innerText = "You lost a point!";
+            p2Status.innerText = "You won a point!";
+            result.innerText = "Lizard beats paper!";
+            break;
+        case (p2Choice == "spock"):
+            p1Status.innerText = "You won a point!";
+            p2Status.innerText = "You lost a point!";
+            result.innerText = "Paper beats Spock!";
+            break;
+        default:
+            console.log("paper was not picked by p1")
+    }
+
+    //p1 picks scissors
+    switch (p1Choice == "scissors") {
+        case (p2Choice == "rock"):
+            p1Status.innerText = "You lost a point!";
+            p2Status.innerText = "You won a point!";
+            result.innerText = "Rock beats Scissors!";
+            break;
+        case (p2Choice == "paper"):
+            p1Status.innerText = "You won a point!";
+            p2Status.innerText = "You lost a point!";
+            result.innerText = "Scissors beats paper!";
+            break;
+        case (p2Choice == "scissors"):
+            result.innerText = "Its a Tie!";
+
+            break;
+        case (p2Choice == "lizard"):
+            p1Status.innerText = "You won a point!";
+            p2Status.innerText = "You lost a point!";
+            result.innerText = "Scissors beats Lizard!";
+            break;
+        case (p2Choice == "spock"):
+            p1Status.innerText = "You lost a point!";
+            p2Status.innerText = "You won a point!";
+            result.innerText = "Spock beats scissors!";
+            break;
+        default:
+            console.log("Scissors was not picked by p1")
+    }
+
+    //p1 picks Lizard
+    switch (p1Choice == "lizard") {
+        case (p2Choice == "rock"):
+            p1Status.innerText = "You lost a point!";
+            p2Status.innerText = "You won a point!";
+            result.innerText = "Rock beats Lizard!";
+            break;
+        case (p2Choice == "paper"):
+            p1Status.innerText = "You won a point!";
+            p2Status.innerText = "You lost a point!";
+            result.innerText = "Paper beats Lizard!";
+            break;
+        case (p2Choice == "scissors"):
+            p1Status.innerText = "You lost a point!";
+            p2Status.innerText = "You won a point!";
+            result.innerText = "Scissors beats Lizard!";
+            break;
+        case (p2Choice == "lizard"):
+            result.innerText = "Its a Tie!";
+
+            break;
+        case (p2Choice == "spock"):
+            p1Status.innerText = "You won a point!";
+            p2Status.innerText = "You lost a point!";
+            result.innerText = "Lizard beats Spock!";
+            break;
+        default:
+            console.log("Lizard was not picked by p1")
+    }
+
+    //p1 picks Spock
+    switch (p1Choice == "spock") {
+        case (p2Choice == "rock"):
+            p1Status.innerText = "You won a point!";
+            p2Status.innerText = "You lost a point!";
+            result.innerText = "Spock beats Rock!";
+            break;
+        case (p2Choice == "paper"):
+            p1Status.innerText = "You lost a point!";
+            p2Status.innerText = "You won a point!";
+            result.innerText = "Paper beats Spock!";
+            break;
+        case (p2Choice == "scissors"):
+            p1Status.innerText = "You won a point!";
+            p2Status.innerText = "You lost a point!";
+            result.innerText = "Spock beats Scissors!";
+            break;
+        case (p2Choice == "lizard"):
+            p1Status.innerText = "You lost a point!";
+            p2Status.innerText = "You won a point!";
+            result.innerText = "Lizard beats Spock!";
+            break;
+        case (p2Choice == "spock"):
+            result.innerText = "Its a Tie!";
+
+            break;
+        default:
+            console.log("Rock was not picked by p1")
+    }
+}
